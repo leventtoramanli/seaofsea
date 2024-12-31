0aae56978486e2a927048a1b84576b71e4cb8d0c
leventtoramanli <42655382+leventtoramanli@users.noreply.github.com>
2024-12-31 10:03:59 +0300

    Auto-sync: Sal 31.12.2024 10:03:59,54

--- public/api/login.php
+++ public/api/login.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 require_once __DIR__ . '/../../lib/handlers/CRUDHandlers.php'; // CRUDHandler için gerekli dosya
 require_once __DIR__ . '/../../vendor/autoload.php'; // JWT ve .env için gerekli
 
